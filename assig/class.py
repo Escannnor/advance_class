1be097f491335a5226d40e956024d7eeb15cde55
unknown <sawdickagboke@gmail.com>
2024-04-05 10:51:20 +0100

    adv

--- assig/class.py
+++ assig/class.py
@@ -1,27 +1,54 @@
 # class
-class CAR:
-    def __init__(self, brand=str, model=str, year=int, color=str):
-        self.brand = brand
-        self.model = model
-        self.year = year
-        self.color = color
+# class CAR:
+#     def __init__(self, brand=str, model=str, year=int, color=str):
+#         self.brand = brand
+#         self.model = model
+#         self.year = year
+#         self.color = color
         
-    def sound(self):
-        return 'zooommm!!!!'
+#     def sound(self):
+#         return 'zooommm!!!!'
     
-motor = CAR('Honda', 'Accord', '2013', 'White')
-print(motor.sound())
+# motor = CAR('Honda', 'Accord', '2013', 'White')
+# print(motor.sound())
         
-class AC:
-    def __init__(self, brand, model, horsepower, color):
-        self.brand = brand
-        self.model = model
-        self.horsepower = horsepower
-        self.color = color
+# class AC:
+#     def __init__(self, brand, model, horsepower, color):
+#         self.brand = brand
+#         self.model = model
+#         self.horsepower = horsepower
+#         self.color = color
         
-    def cold(self):
-        return 'weather'
+#     def cold(self):
+#         return 'weather'
 
-aircon = AC('lg', 'v2', '50', 'yyellow')
-print(aircon.cold())   
+# aircon = AC('lg', 'v2', '50', 'yyellow')
+# print(aircon.cold())   
+
+class Fish:
+    def swim(self):
+        pass
+    
+class Titus(Fish):
+    def swim(self):
+        print('water')
+        
+class Tilapia(Fish):
+    def swim(self):
+        print('river')
+        
+def ocean(fish):
+    fish.swim()
+    
+titus = Titus()
+tilapia = Tilapia()
+
+ocean(titus)
+ocean(tilapia)
+
+
+        
+
+
+    
       
